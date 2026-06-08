f89de2b2ab65403d8da82981da339de1cdaa589a
fathur-md <fathurmuh@outlook.com>
2026-06-08 12:20:15 +0700

    feat(index): add initial PHP navigation page

---
+++ apps/php/index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>PHP Navigation</title>
+  <script src="https://cdn.tailwindcss.com"></script>
+</head>
+
+<body class="min-h-dvh flex flex-col bg-gray-100">
+  <header class="max-w-6xl mx-auto w-full p-5">
+    <nav class="shadow rounded-lg p-5">
+      <a class="bg-gray-200 p-3 rounded-lg" href="./latihan">Latihan DIR</a>
+      <a class="bg-gray-200 p-3 rounded-lg" href="./tugas/tugas-login.php">Tugas Login</a>
+    </nav>
+  </header>
+</body>
+
+</html>
